6f4794217361b030e7ec918689f81b2c7edeab4c
Aaljin <aaljin.dev@gmail.com>
2026-05-15 14:39:49 +0530

    last push

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 const App = () => {
   const [task, setTask] = useState([]);
   const [input, setInput] = useState("");
-  const [editId,setEditId] = useState(null);
-  const [editbutton,setEditButton] = useState(false)
+  const [editId, setEditId] = useState(null);
+  const [editbutton, setEditButton] = useState(false);
+  const [open, setOpen] = useState(null);
 
   const handleSubmit = () => {
     if (input.trim() === "") {
@@ -16,57 +17,74 @@ const App = () => {
     let newTask = {
       id: find,
       task: input,
-      status: false,
+      status: "pending",
     };
 
     setInput("");
-    setTask([...task, newTask]);
-    
+    let set = [...task, newTask];
+    localSetFunction(set);
   };
 
-  function handleEditFun(){
-
-    const result = task.map((tasks)=>{
-      if(tasks.id === editId ){
-        return {...tasks,task : input}
+  function handleEditFun() {
+    const result = task.map((tasks) => {
+      if (tasks.id === editId) {
+        return { ...tasks, task: input };
       }
-      return tasks
-    })
-    setTask(result)
-    setEditButton(false)
-    setInput(" ")
-    setEditId(null)
+      return tasks;
+    });
+    setTask(result);
+    console.log(result);
+
+    setEditButton(false);
+    setInput("");
+    setEditId(null);
+    localSetFunction(result);
   }
-  
+  const setFun = () => {};
 
   const handleDelete = (id) => {
     console.log(id);
     const deleteTask = task.filter((tasks) => {
       return tasks.id !== id;
     });
-    console.log(deleteTask);
-    setTask(deleteTask);
+
+    // setTask(deleteTask);
+    localSetFunction(deleteTask);
   };
 
   const handleEdit = (id) => {
     console.log(id);
-    const editTask = task.find((tasks)=>tasks.id === id)
-    setEditId(id)
+    const editTask = task.find((tasks) => tasks.id === id);
+    setEditId(id);
     console.log(editTask.task);
-    setInput(editTask.task)
-    setEditButton(true)
+    setInput(editTask.task);
+    setEditButton(true);
   };
 
-
-  const handleUpdate = (id) => {
-    const taskUpdate = task.map((item) => {
-      if (item.id === id) {
-        return { ...item, status: !item.status };
+  const handleUpdate = (id, value) => {
+    const result = task.map((tasks) => {
+      if (tasks.id === id) {
+        return { ...tasks, status: value };
       }
-      return item;
+      return tasks;
     });
-    setTask(taskUpdate);
+
+    localSetFunction(result);
+    setOpen(null);
   };
+
+  const localSetFunction = (data) => {
+    localStorage.setItem("aaljin", JSON.stringify(data));
+    localGetFunction();
+  };
+  const localGetFunction = () => {
+    const data = localStorage.getItem("aaljin");
+    console.log(data);
+    setTask(JSON.parse(data));
+  };
+  useEffect(() => {
+    localGetFunction();
+  }, []);
 
   return (
     <div>
@@ -83,63 +101,99 @@ const App = () => {
         <button
           className=" text-white w-22 h-10 px-10 py-2 rounded"
           onClick={editbutton ? handleEditFun : handleSubmit}
-          className ={editId !==null ? "bg-pink-500 text-white w-22 h-10 px-10 py-2 rounded" 
-                                    : "bg-blue-500 text-white w-22 h-10 px-10 py-2 rounded"}
+          className={
+            editId !== null
+              ? "bg-pink-500 text-white w-22 h-10 px-10 py-2 rounded"
+              : "bg-blue-500 text-white w-22 h-10 px-10 py-2 rounded"
+          }
         >
-          {editId !==null ? "Edit" : "Sumbit"}
+          {editId !== null ? "Edit" : "Sumbit"}
         </button>
       </section>
       <section className="w-full flex justify-center">
         <table className=" w-8/12 min-h-[66px] text-center  ">
-        <tbody>
-        {task.map((item) => (
-            
-              <tr className="grid grid-cols-[10%_70%_10%_10%] "
-            key={item.id}
-            
-          >
-            <td><input
-              type="checkbox"
-              checked={item.status}
-              onChange={() => handleUpdate(item.id)}
-              className="w-5 h-5 accent-blue-500"
-            /></td>
-            <td>
-              <p
-              className="text-3xl "
-              onClick={() => {
-                handleUpdate(item.id);
-              }}
-            >
-              {item.task}
-            </p>
-            </td>
-            <td>
-              <button
-              className="bg-pink-500 text-white w-22 h-10 px-10 py-2 rounded"
-              onClick={() => {
-                handleEdit(item.id);
-              }}
-            >
-              Edit
-            </button>
-            </td>
-            <td>
-              <button
-              className="bg-orange-500 text-white w-22 h-10 px-10 py-2 rounded"
-              onClick={() => {
-                handleDelete(item.id);
-              }}
-            >
-              Delete
-            </button>
-            </td>
-          </tr>
-          
-        
-        ))}
-        </tbody>
-      </table>
+          <tbody>
+            {task.map((item) => (
+              <tr
+                className="grid grid-cols-[10%_70%_10%_10%] h-15"
+                key={item.id}
+              >
+                <td>
+                  <div className="relative inline-block ">
+                    {/* Button */}
+                    <button
+                      onClick={() => setOpen(open === item.id ? null : item.id)}
+                      className={
+                        item.status === "pending"
+                          ? "bg-yellow-500 text-white w-30 h-8 px-20 py-5 rounded-lg"
+                          : item.status === "In progress"
+                            ? "bg-green-500 text-white w-30 h-8 px-20 py-5 rounded-lg"
+                            : "bg-red-500 text-white w-30 h-8 px-20 py-5 rounded-lg"
+                      }
+                    >
+                      {item.status}
+                    </button>
+
+                    {/* Menu */}
+                    {open === item.id && (
+                      <div className="absolute left-3 mt-10 w-40   bg-white rounded-lg shadow-lg border overflow-hidden z-50">
+                        <button
+                          className="block w-full text-center border h-10 p-5  bg-yellow-500"
+                          onClick={() => {
+                            handleUpdate(item.id, "pending");
+                          }}
+                        >
+                          Pending
+                        </button>
+
+                        <button
+                          className="block w-full text-center border h-10 p-5  bg-green-500"
+                          onClick={() => {
+                            handleUpdate(item.id, "In progress");
+                          }}
+                        >
+                          In Progress
+                        </button>
+
+                        <button
+                          className="block w-full text-center border h-10 p-5 bg-red-500"
+                          onClick={() => {
+                            handleUpdate(item.id, "Completed");
+                          }}
+                        >
+                          Completed
+                        </button>
+                      </div>
+                    )}
+                  </div>
+                </td>
+                <td>
+                  <p className="text-3xl ">{item.task}</p>
+                </td>
+                <td>
+                  <button
+                    className="bg-pink-500 text-white w-22 h-10 px-10 py-2 rounded"
+                    onClick={() => {
+                      handleEdit(item.id);
+                    }}
+                  >
+                    Edit
+                  </button>
+                </td>
+                <td>
+                  <button
+                    className="bg-orange-500 text-white w-22 h-10 px-10 py-2 rounded"
+                    onClick={() => {
+                      handleDelete(item.id);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </section>
     </div>
   );
